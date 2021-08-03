c16c1c91ac86da251d97f5f38df728795f452b06
PipeWolox <felipe.f.rivera@accenture.com>
2021-08-03 09:13:51 -0500

    fixed changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { useSelector } from 'contexts/UserContext';
 import PATHS from 'components/Routes/paths';
@@ -9,15 +9,11 @@ import PATHS from 'components/Routes/paths';
 import itemsMenu from './constants';
 import styles from './styles.module.scss';
 
-const classLink = (actualPath: string, path: string) =>
-  actualPath === path ? `${styles.link} ${styles.active}` : styles.link;
-
 function Header() {
   const { t } = useTranslation();
   const user = useSelector((state) => state.user);
 
   const items = itemsMenu.filter(({ isProtected }) => (user && isProtected) || (!user && !isProtected));
-  const { pathname } = useLocation();
 
   return (
     <div className={styles.contentHeader}>
@@ -25,17 +21,17 @@ function Header() {
       <div>
         <ul className={styles.listItems}>
           <li className={styles.item}>
-            <Link to={PATHS.home} className={classLink(pathname, PATHS.home)}>
+            <NavLink to={PATHS.home} activeClassName={styles.active} className={styles.link}>
               <FontAwesomeIcon icon={faHome} size="xs" className={styles.icon} />
               <span className={styles.text}>{t('Header:home')}</span>
-            </Link>
+            </NavLink>
           </li>
           {items.map(({ text, href, icon }) => (
             <li className={styles.item} key={href}>
-              <Link to={href} className={classLink(pathname, href)}>
+              <NavLink to={href} activeClassName={styles.active} className={styles.link}>
                 <FontAwesomeIcon icon={icon} size="xs" className={styles.icon} />
                 <span className={styles.text}>{t(text)}</span>
-              </Link>
+              </NavLink>
             </li>
           ))}
         </ul>
