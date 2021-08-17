dc3a97280d93c3c8bd2c002b6fbb8f26ecfa7dcb
Hernan Clich <hernan.clich@wolox.com.ar>
2021-08-17 10:29:13 -0300

    Added article text content to the articles/:slug screen

---
+++ src/screens/Article/components/ArticleContent/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+
+interface Props {
+  textContent: string;
+}
+
+function ArticleContent({ textContent }: Props) {
+  return (
+    <article className="full-width">
+      <div className={styles.container}>
+        <p className={styles.content}>{textContent}</p>
+      </div>
+    </article>
+  );
+}
+
+export default ArticleContent;
--- src/screens/Article/index.tsx
+++ src/screens/Article/index.tsx
@@ -1,12 +1,14 @@
 import Header from 'components/Header';
 
 import ArticleBanner from './components/ArticleBanner';
+import ArticleContent from './components/ArticleContent';
 
 function Article() {
   return (
     <>
       <Header />
       <ArticleBanner bannerData={{ articleDate: 'Sun Aug 01 2021', userName: 'SuperUser' }} />
+      <ArticleContent textContent="Bacon ipsum dolor amet swine prosciutto ribeye andouille tri-tip corned beef strip steak ball tip meatloaf buffalo shank fatback filet mignon porchetta. Tongue landjaeger frankfurter shank, porchetta pork loin beef prosciutto cow doner strip steak sirloin chuck leberkas turkey. Sirloin leberkas jowl venison cupim pancetta prosciutto boudin. Tail meatball bacon, pork loin doner fatback alcatra jerky pastrami kielbasa. Ground round capicola salami pork buffalo. Prosciutto ham hock beef pork brisket ham t-bone beef ribs tenderloin. Bacon sirloin turkey shankle." />
     </>
   );
 }
