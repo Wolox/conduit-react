9ee956756228b5b3a42ebac230139d2f48158923
Renzo Torres <renzo.torres@wolox.com.ar>
2021-08-18 13:59:21 -0300

    Login - fix tests

--- src/screens/Dashboard/screens/Home/index.test.tsx
+++ src/screens/Dashboard/screens/Home/index.test.tsx
@@ -6,8 +6,6 @@ import * as AuthService from 'services/AuthService';
 
 import Home from '.';
 
-const reactI18next = jest.requireMock('react-i18next');
-
 const renderHome = () => {
   render(
     <Router>
@@ -50,44 +48,5 @@ describe('Home component', () => {
     userEvent.type(screen.getByPlaceholderText(/Home:newTech/), '');
     await waitFor(() => userEvent.click(screen.getByRole('button', { name: /Home:setNewTech/ })));
     expect(screen.queryByText(/Home:techIs {"tech":"React"}/)).toBeInTheDocument();
-  });
-
-  test('Calls change language from es to en', async () => {
-    const changeLanguage = jest.fn();
-    const spy = jest.spyOn(reactI18next, 'useTranslation').mockReturnValue({
-      t: (key: string) => key,
-      i18n: {
-        changeLanguage,
-        language: 'es'
-      }
-    });
-
-    const { rerender } = render(
-      <Router>
-        <Home />
-      </Router>
-    );
-
-    const logoutButton = screen.getByRole('button', { name: /Home:changeLang/ });
-    // Calls once for first lang change to "en"
-    userEvent.click(logoutButton);
-    await waitFor(() => expect(changeLanguage).toHaveBeenCalledWith('en'));
-    spy.mockClear();
-
-    spy.mockReturnValue({
-      t: (key: string) => key,
-      i18n: {
-        changeLanguage,
-        language: 'en'
-      }
-    });
-
-    // Rerender the component again to take the new spy mock
-    renderHome();
-    // Calls a second time to change lang to return lang to "es"
-    userEvent.click(logoutButton);
-    await waitFor(() => expect(changeLanguage).toHaveBeenCalledWith('en'));
-
-    spy.mockClear();
   });
 });
