5fd8ce8b166280ebe3efcd3507e7ef56807cfdaa
Renzo Torres <renzo.torres@wolox.com.ar>
2021-08-18 13:50:14 -0300

    Login - tests

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,7 +6,8 @@ import api, { STATUS_CODES } from 'config/api';
 import { userProfileMock } from './user';
 
 const handlers = [
-  rest.post('/users', (req, res, ctx) => res(ctx.status(STATUS_CODES.ok), ctx.json(userProfileMock)))
+  rest.post('/users', (req, res, ctx) => res(ctx.status(STATUS_CODES.ok), ctx.json(userProfileMock))),
+  rest.post('/users/login', (req, res, ctx) => res(ctx.status(STATUS_CODES.ok), ctx.json(userProfileMock)))
 ];
 
 export { handlers, rest, api };
