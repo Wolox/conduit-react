763847e335c623f78c8cd61096ee84a480f3bddb
hernan-clich <cervece41@gmail.com>
2021-09-02 11:55:59 -0300

    Using the real user object to validate if user is logged in in ArticleComments

--- src/screens/Article/components/ArticleComments/index.tsx
+++ src/screens/Article/components/ArticleComments/index.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'contexts/UserContext';
 import { MOCKED_COMMENTS, MOCKED_CURRENT_USER } from 'screens/Article/constants';
 
 import ArticleComment from './components/ArticleComment';
@@ -5,7 +6,8 @@ import ArticleCommentForm from './components/ArticleCommentForm';
 import ArticlePrompt from './components/ArticlePrompt';
 
 function ArticleComments() {
-  const isLoggedIn = false;
+  const user = useSelector((state) => state.user);
+  const isLoggedIn = !!user?.token;
   const doesArticleHaveComments = !!MOCKED_COMMENTS && !!MOCKED_COMMENTS.length;
 
   return (
