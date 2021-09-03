f0bd0351bebf507bbffed5858bbfcd2f0edc823d
hernan-clich <cervece41@gmail.com>
2021-09-03 12:59:15 -0300

    Added trash can icon to single comments

--- src/screens/Article/components/ArticleComments/components/ArticleComment/index.tsx
+++ src/screens/Article/components/ArticleComments/components/ArticleComment/index.tsx
@@ -1,5 +1,7 @@
 import { generatePath, Link } from 'react-router-dom';
 import cn from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 import userPlaceholder from 'assets/user-placeholder.jpeg';
 import PATHS from 'components/Routes/paths';
@@ -12,21 +14,29 @@ type Props = {
 };
 
 function ArticleComment({ commentData }: Props) {
+  const ICON_SIZE = 'xs';
   const { author, body, updatedAt } = commentData;
   const { username, image } = author;
   const userToRedirect = generatePath(PATHS.user, { username });
 
+  const handleClick = () => null;
+
   return (
     <div className={styles.cardContainer}>
       <p className={styles.cardComment}>{body}</p>
-      <div className={cn('row middle', styles.cardFooter)}>
-        <Link to={userToRedirect}>
-          <img className={styles.userIcon} src={image || userPlaceholder} alt={username} />
-        </Link>
-        <Link className={styles.userLink} to={userToRedirect}>
-          {username}
-        </Link>
-        <span className={styles.date}>{updatedAt}</span>
+      <div className={cn('row middle space-between', styles.cardFooter)}>
+        <div className="row middle">
+          <Link to={userToRedirect}>
+            <img className={styles.userIcon} src={image || userPlaceholder} alt={username} />
+          </Link>
+          <Link className={styles.userLink} to={userToRedirect}>
+            {username}
+          </Link>
+          <span className={styles.date}>{updatedAt}</span>
+        </div>
+        <button type="button" onClick={handleClick}>
+          <FontAwesomeIcon icon={faTrashAlt} size={ICON_SIZE} className={styles.icon} />
+        </button>
       </div>
     </div>
   );
