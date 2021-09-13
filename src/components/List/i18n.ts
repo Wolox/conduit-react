ed17aae30c6e0683ae795b04689cb39782e2a34b
PipeWolox <felipe.f.rivera@accenture.com>
2021-09-13 14:53:26 -0500

    Remove Component List

--- src/components/List/i18n.ts
+++
@@ -1,9 +0,0 @@
-import i18next from 'i18next';
-
-i18next.addResources('es', 'List', {
-  empty: 'No hay artículos aquí... todavía'
-});
-
-i18next.addResources('en', 'List', {
-  empty: 'No articles are here... yet'
-});
